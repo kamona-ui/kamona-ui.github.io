03d37dddc81f8a2f84c4067c1871c7e4d24b650b
Ahmed Kamel <ak384909@gmail.com>
2023-06-07 16:33:21 +0300

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import NProgress from 'nprogress'
 import routes from '@/router/routes'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
